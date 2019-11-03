b3f5baa8210fe2a9d77a0b11d84dd8081268436f
rfviolato <rfviolato@gmail.com>
2019-11-03 22:20:15 +0100

    feat: add home leave log function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,36 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import * as moment from 'moment';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+const db = admin.database();
+
+export const logHomeLeave = functions.https.onRequest(
+  async (request, response) => {
+    const {
+      body: { date }
+    } = request;
+
+    console.log({ date });
+
+    if (!date) {
+      return response.status(400).send(new Error('Date is missing!'));
+    }
+
+    const momentDate = moment(date);
+    const today = momentDate.format('DD-MM-YYYY');
+    const homeLeaveTime = momentDate.format('hh:mm:ss');
+
+    try {
+      await db
+        .ref('/')
+        .child(today)
+        .set({ homeLeaveTime });
+
+      return response.status(200).send();
+    } catch (exception) {
+      return response.status(500).send(new Error(exception));
+    }
+  }
+);
