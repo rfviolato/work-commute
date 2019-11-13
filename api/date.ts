ea9cadae87e2bb3def5687ec7c552f00ec8ab6bd
rfviolato <rfviolato@gmail.com>
2019-11-13 08:49:13 +0100

    feat: change folder structure

--- api/date.ts
+++ api/date.ts
@@ -1,5 +1,5 @@
 import { NowRequest, NowResponse } from '@now/node';
 
 export default (req: NowRequest, res: NowResponse) => {
-  res.json({ name: 'John', email: 'john@example.com' });
+  res.send(new Date());
 };
--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React from "react";
-import "./App.css";
+import React, { useEffect, useState } from 'react';
+import './App.css';
 
 function App() {
-  return <div className="App">Hello World!</div>;
+  const [date, setDate] = useState(null);
+
+  useEffect(() => {
+    async function fetchDate() {
+      const data = await fetch('http://localhost:3000/api/date');
+      const result = await data.text();
+
+      setDate(result);
+    }
+
+    fetchDate();
+  }, []);
+  return <div className="App">Hello World! {date}</div>;
 }
 
 export default App;
