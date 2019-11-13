ecaad00f3a7f31daf2fdcb34b91c09b00d77fce1
rfviolato <rfviolato@gmail.com>
2019-11-13 21:31:33 +0100

    feat: now dev with CRA hot-reloading

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,26 +1,25 @@
+import { hot } from 'react-hot-loader/root';
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [date, setDate] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch('/api/date')
+      .then(response => response.text())
+      .then(text => setDate(text));
+  }, [setDate]);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Current date from API: {date}</p>
       </header>
     </div>
   );
 }
 
-export default App;
+export default process.env.NODE_ENV === 'development' ? hot(App) : App;
