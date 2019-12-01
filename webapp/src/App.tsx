c6e566d89c94bca358add9251e2f55d3f6e28f16
rfviolato <rfviolato@gmail.com>
2019-12-01 18:32:43 +0100

    chore: add "font-awesome"

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { hot } from 'react-hot-loader/root';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/pro-regular-svg-icons';
 
 const App: React.FC = () => {
-  return <div>hell yea!</div>;
+  return (
+    <div>
+      hell yea! <FontAwesomeIcon icon={faTimes} />
+    </div>
+  );
 };
 
 export default process.env.NODE_ENV === 'development' ? hot(App) : App;
