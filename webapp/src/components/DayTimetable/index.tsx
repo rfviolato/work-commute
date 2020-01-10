eaa2795a47258f11dc5df5a2d3dce0b765012700
Rafael Violato <rafael.violato@ah.nl>
2020-01-10 14:26:04 +0100

    feat: handle query error on "DayTimetable"

--- webapp/src/components/DayTimetable/index.tsx
+++ webapp/src/components/DayTimetable/index.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import moment from 'moment';
 import { faClock } from '@fortawesome/pro-regular-svg-icons';
 import { useQuery } from '@apollo/react-hooks';
+import styled from '@emotion/styled';
 import { TimetableDisplay } from '../TimetableDisplay';
 import { DEVELOPMENT_DAY, IS_DEV } from '../../constants';
 import query from './query';
 import { IDayTimetableQuery } from './interface';
+import { QueryErrorIcon } from '../QueryErrorIcon';
 
 const LABELS = {
   HOME_LEAVE: 'Home leave time',
@@ -14,9 +16,15 @@ const LABELS = {
   WORK_ARRIVE: 'Home arrive time',
 };
 
+const QueryErrorContainer = styled.div`
+  position: absolute;
+  top: 10px;
+  left: 10px;
+`;
+
 export const DayTimetable: React.FC = () => {
   const day = IS_DEV ? DEVELOPMENT_DAY : moment().format('YYYY-MM-DD');
-  const { loading, data } = useQuery<IDayTimetableQuery>(query, {
+  const { loading, data, error } = useQuery<IDayTimetableQuery>(query, {
     variables: { day },
   });
 
@@ -39,15 +47,23 @@ export const DayTimetable: React.FC = () => {
   }
 
   return (
-    <TimetableDisplay
-      icon={faClock}
-      isLoading={loading}
-      timetables={[
-        { label: LABELS.HOME_LEAVE },
-        { label: LABELS.WORK_ARRIVE },
-        { label: LABELS.WORK_LEAVE },
-        { label: LABELS.HOME_ARRIVE },
-      ]}
-    />
+    <>
+      {error && (
+        <QueryErrorContainer>
+          <QueryErrorIcon />
+        </QueryErrorContainer>
+      )}
+
+      <TimetableDisplay
+        icon={faClock}
+        isLoading={loading}
+        timetables={[
+          { label: LABELS.HOME_LEAVE },
+          { label: LABELS.WORK_ARRIVE },
+          { label: LABELS.WORK_LEAVE },
+          { label: LABELS.HOME_ARRIVE },
+        ]}
+      />
+    </>
   );
 };
