d1cf490655cf6cf47cd3eb8ca14de836f6beeb82
Rafael Violato <rafael.violato@ah.nl>
2020-01-12 18:59:33 +0100

    feat: improve layout for navigation when item is active

--- webapp/src/components/Navigation/index.tsx
+++ webapp/src/components/Navigation/index.tsx
@@ -8,7 +8,7 @@ const Root = styled.nav`
   justify-content: center;
   width: 100%;
   height: 50px;
-  background-color: #434343;
+  background-color: #404040;
   border-bottom: 1px solid #aaa;
 `;
 
@@ -17,17 +17,48 @@ const Link = styled(NavLink)`
     color: currentColor;
   }
 
+  position: relative;
+  top: -2px;
+  opacity: 0.6;
   color: #fff;
   text-decoration: none;
   font-size: 20px;
   transition: opacity 300ms ease-out;
+
+  &::before,
+  &::after {
+    content: '';
+    position: absolute;
+    width: 55%;
+    height: 1px;
+    bottom: -2px;
+    background-color: currentColor;
+    opacity: 0.85;
+    transform: scaleX(0);
+    transition: transform 300ms ease-out;
+  }
+
+  &::before {
+    right: -5%;
+    transform-origin: left;
+  }
+
+  &::after {
+    left: -5%;
+    transform-origin: right;
+  }
 
   &:not(:last-child) {
     margin-right: 30px;
   }
 
   &.active {
-    opacity: 0.5;
+    opacity: 1;
+
+    &::before,
+    &::after {
+      transform: scaleX(1);
+    }
   }
 `;
 
