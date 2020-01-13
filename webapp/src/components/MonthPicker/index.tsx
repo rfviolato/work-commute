e5351426e50ddffe53eff01c37ac89cd8b04d21b
Rafael Violato <rafael.violato@ah.nl>
2020-01-13 21:01:26 +0100

    feat: make sure to start at the correct year when setting period via query string

--- webapp/src/components/MonthPicker/index.tsx
+++ webapp/src/components/MonthPicker/index.tsx
@@ -286,84 +286,84 @@ export const MonthPickerComponent: React.FC<IMonthPickerComponentProps> = ({
   );
 
   React.useEffect(() => {
-    const startDate = moment(`${minYear}-${minMonth}`, 'YYYY-MM');
-    const endDate = moment(`${maxYear}-${maxMonth}`, 'YYYY-MM');
-    const monthCount = endDate.diff(startDate, 'months') + 1;
-    const yearCount = parseInt(maxYear, 10) - parseInt(minYear, 10) + 1;
-    const yearsStartDate = moment(startDate).subtract(1, 'year');
+    if (!isLoading) {
+      const startDate = moment(`${minYear}-${minMonth}`, 'YYYY-MM');
+      const endDate = moment(`${maxYear}-${maxMonth}`, 'YYYY-MM');
+      const monthCount = endDate.diff(startDate, 'months') + 1;
+      const yearCount = parseInt(maxYear, 10) - parseInt(minYear, 10) + 1;
+      const yearsStartDate = moment(startDate).subtract(1, 'year');
 
-    const years = Array(yearCount)
-      .fill(yearCount)
-      .reduce(
-        (accum: {}) => ({
-          ...accum,
-          [yearsStartDate.add(1, 'year').format('YYYY')]: [],
-        }),
-        {},
-      );
-    const yearList = Object.keys(years);
+      const years = Array(yearCount)
+        .fill(yearCount)
+        .reduce(
+          (accum: {}) => ({
+            ...accum,
+            [yearsStartDate.add(1, 'year').format('YYYY')]: [],
+          }),
+          {},
+        );
+      const yearList = Object.keys(years);
 
-    const monthsStartDate = moment(startDate).subtract(1, 'month');
-    const monthsPerYear: IMonthsPerYear = Array(monthCount)
-      .fill(monthCount)
-      .map(() => {
-        const date = monthsStartDate.add(1, 'month');
+      const monthsStartDate = moment(startDate).subtract(1, 'month');
+      const monthsPerYear: IMonthsPerYear = Array(monthCount)
+        .fill(monthCount)
+        .map(() => {
+          const date = monthsStartDate.add(1, 'month');
 
-        return {
-          year: date.format('YYYY'),
-          month: date.format('MM'),
-        };
-      })
-      .reduce(
-        (
-          accum: { [key: string]: string[] },
-          { year, month }: { year: string; month: string },
-        ) => {
+          return {
+            year: date.format('YYYY'),
+            month: date.format('MM'),
+          };
+        })
+        .reduce(
+          (
+            accum: { [key: string]: string[] },
+            { year, month }: { year: string; month: string },
+          ) => {
+            return {
+              ...accum,
+              [year]: [...accum[year], month],
+            };
+          },
+          years,
+        );
+
+      const calendarMonthsStartDate = moment('12-01', 'MM-DD'); // starts at December because first iteration already add 1 month
+      const calendarMonthsPerYear: ICalendarMonthPerYear = yearList.reduce(
+        (accum: { [key: string]: any }, year: string) => {
           return {
             ...accum,
-            [year]: [...accum[year], month],
+            [year]: Array(12)
+              .fill(12)
+              .map(() => {
+                const date = calendarMonthsStartDate.add(1, 'month');
+                const month = date.format('MM');
+                const isAvailable = monthsPerYear[year].includes(month);
+
+                return {
+                  month,
+                  text: date.format('MMM').toUpperCase(),
+                  isAvailable,
+                };
+              }),
           };
         },
-        years,
+        {},
       );
 
-    const calendarMonthsStartDate = moment('12-01', 'MM-DD'); // starts at December because first iteration already add 1 month
-    const calendarMonthsPerYear: ICalendarMonthPerYear = yearList.reduce(
-      (accum: { [key: string]: any }, year: string) => {
-        return {
-          ...accum,
-          [year]: Array(12)
-            .fill(12)
-            .map(() => {
-              const date = calendarMonthsStartDate.add(1, 'month');
-              const month = date.format('MM');
-              const isAvailable = monthsPerYear[year].includes(month);
+      setAvailableYearList(yearList);
+      setCalendarMonthLabels(calendarMonthsPerYear);
 
-              return {
-                month,
-                text: date.format('MMM').toUpperCase(),
-                isAvailable,
-              };
-            }),
-        };
-      },
-      {},
-    );
+      const availableYearIndex = yearList.indexOf(browsingYear);
+      const isYearAvailable = availableYearIndex >= 0;
 
-    setAvailableYearList(yearList);
-    setCalendarMonthLabels(calendarMonthsPerYear);
-  }, [minYear, maxYear, minMonth, maxMonth, isLoading]);
+      if (!isYearAvailable) {
+        const latestAvailableYearIndex = yearList.length - 1;
 
-  React.useEffect(() => {
-    const availableYearIndex = availableYearList.indexOf(browsingYear);
-    const isYearAvailable = availableYearIndex >= 0;
-
-    if (!isYearAvailable) {
-      const latestAvailableYearIndex = availableYearList.length - 1;
-
-      setBrowsingYear(availableYearList[latestAvailableYearIndex]);
+        setBrowsingYear(yearList[latestAvailableYearIndex]);
+      }
     }
-  }, [availableYearList]);
+  }, [minYear, maxYear, minMonth, maxMonth, isLoading]);
 
   return (
     <Root onMouseLeave={onComponentLeave}>
