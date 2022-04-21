d5dc2feefca45b1eff23f534d934b336d7d6e762
rfviolato <rfviolato@gmail.com>
2022-04-21 21:34:08 +0200

    :zap: Import minified css files

--- webapp/src/index.tsx
+++ webapp/src/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import '@rfviolato/private-registry/font-awesome-5/css/fontawesome.css';
-import '@rfviolato/private-registry/font-awesome-5/css/solid.css';
-import '@rfviolato/private-registry/font-awesome-5/css/regular.css';
+import '@rfviolato/private-registry/font-awesome-5/css/fontawesome.min.css';
+import '@rfviolato/private-registry/font-awesome-5/css/solid.min.css';
+import '@rfviolato/private-registry/font-awesome-5/css/regular.min.css';
 
 // fonts
 import 'typeface-roboto';
